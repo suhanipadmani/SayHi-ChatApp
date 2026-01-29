601b015d05d6c74d7ec910e10613f11917ecb672
suhanipadmani <suhanipadmani0903@gmail.com>
2026-01-29 12:49:12 +0530

    commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: "none", // CSRF attacks
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
