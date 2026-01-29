97231456d680edb94af6bf02b21f96205b3c17f5
suhanipadmani <suhanipadmani0903@gmail.com>
2026-01-29 12:18:22 +0530

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ if (ENV.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/dist");
   app.use(express.static(frontendPath));
 
-  app.get("/:anyPath(*)", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
