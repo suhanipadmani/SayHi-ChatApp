2a6f62d1a8d43a44e9d2fa9d08e251dd7b14f8db
suhanipadmani <suhanipadmani0903@gmail.com>
2026-01-29 13:24:19 +0530

    commit changes

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -24,7 +24,12 @@ function LoginPage() {
                   {/* HEADING TEXT */}
                   <div className="text-center mb-8">
                     <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
-                    <h2 className="text-2xl font-bold text-slate-200 mb-2">Ready to SayHi again?</h2>
+                    <h2 className="text-2xl font-bold text-slate-200 mb-2">Ready to 
+                      <span className=" text-cyan-200"> Say
+                        <span className="text-cyan-400">Hi </span>
+                      </span>
+                       again?
+                    </h2>
                     <p className="text-slate-400">Login to access to your account</p>
                   </div>
 
